c573b135170c3641b5406fc98038e9148a5f4c14
Vincent Gibara <vgibara@gmail.com>
2026-01-25 12:46:01 -0500

    Meilleure gestion des connexions sécurisées rediss://

--- src/queue/connection.ts
+++ src/queue/connection.ts
@@ -3,4 +3,15 @@ import { env } from '../config/env.js';
 
 export const connection: ConnectionOptions = {
   url: env.REDIS_URL,
+  // Digital Ocean Managed Redis and other providers often require TLS
+  ...(env.REDIS_URL.startsWith('rediss://')
+    ? {
+        redis: {
+          tls: {
+            rejectUnauthorized: false, // Useful for self-signed or internal DO certificates
+          },
+        },
+      }
+    : {}),
 };
+
